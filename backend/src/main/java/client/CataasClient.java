17caef35d2dce6e87c29f31116f883721cf17c05
DennisDHBW <den.gerhardt.24@lehre.mosbach.dhbw.de>
2026-01-26 17:05:29 +0100

    Add Client for Cataas

---
+++ backend/src/main/java/client/CataasClient.java
@@ -0,0 +1,20 @@
+package client;
+
+import dto.CataasResponseDTO;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
+
+@Path("/cat")
+@RegisterRestClient(configKey = "cataas-api")
+public interface CataasClient {
+
+    @GET
+    @Path("/{tag}")
+    CataasResponseDTO getCatByTag(
+            @PathParam("tag") String tag,
+            @QueryParam("json") boolean json
+    );
+}
